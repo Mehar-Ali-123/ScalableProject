b76053f6181f8d0c6c72eedc04b95ee6449bae4f
Mehar-Ali-123 <alihussniian0306@gmail.com>
2025-05-09 02:54:50 +0100

    backend link server add

--- InstagramProjectScalableFrontend/src/server.js
+++ InstagramProjectScalableFrontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2/user";
-export const backend_url = "http://localhost:8000";
+// export const server = "http://localhost:8000/api/v2/user";
+// export const backend_url = "http://localhost:8000";
 
-// export const server = "https://api.carbonshredder.com/api/v2/user";
-// export const backend_url = "https://api.carbonshredder.com/";
+export const server = "https://instaulster-brfpbvbra9e8cddj.canadacentral-01.azurewebsites.net/api/v2/user";
+export const backend_url = "https://instaulster-brfpbvbra9e8cddj.canadacentral-01.azurewebsites.net";
